aa1dffbf3ddd9adf5e21543262ba0908d38c06e7
zozyo <zzy.touhou@gmail.com>
2024-05-01 13:43:44 +0800

    Update delete-worker-v3.sh

--- delete-worker-v3.sh
+++ delete-worker-v3.sh
@@ -4,7 +4,7 @@ for LINE in `cat ipList.csv`;do
 
 	DATARAW=$(echo  '{"RemoveWorker":{"name":"'"${NAME}"'"}}')
 	echo $DATARAW
-        curl 'http://127.0.0.1:3000/api/p/http%3A%2F%2F127.0.0.1%3A3001/wm/config' \
+        curl 'http://127.0.0.1:3001/wm/config' \
   -H 'Content-Type: application/json' \
   --data-raw ${DATARAW}
 
